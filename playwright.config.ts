3fb5dfb2c1c63588f7c59094ea358d2145fabd3d
Ishikawa Ryuto <noreply@rishikawa.net>
2026-02-14 08:17:02 +0900

    テスト時にスクリンショットと動画を記録する

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,21 +1,21 @@
-import { defineConfig, devices } from '@playwright/test';
+import { defineConfig, devices } from "@playwright/test";
 
-const includeWebkit = process.env.PW_INCLUDE_WEBKIT === '1';
+const includeWebkit = process.env.PW_INCLUDE_WEBKIT === "1";
 
 const projects = [
   {
-    name: 'chromium',
-    use: { ...devices['Desktop Chrome'] },
+    name: "chromium",
+    use: { ...devices["Desktop Chrome"] },
   },
   {
-    name: 'firefox',
-    use: { ...devices['Desktop Firefox'] },
+    name: "firefox",
+    use: { ...devices["Desktop Firefox"] },
   },
   ...(includeWebkit
     ? [
         {
-          name: 'webkit',
-          use: { ...devices['Desktop Safari'] },
+          name: "webkit",
+          use: { ...devices["Desktop Safari"] },
         },
       ]
     : []),
@@ -33,7 +33,7 @@ const projects = [
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: './tests',
+  testDir: "./tests",
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -43,14 +43,19 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: "on-first-retry",
+    screenshot: "on",
+    video: {
+      mode: "on",
+      size: { width: 640, height: 480 },
+    },
   },
 
   /* Configure projects for major browsers */
